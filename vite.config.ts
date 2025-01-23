9bfd888d95dd8d034152ae30bb8005b70acad8aa
benjaminkent <benjamin.jehl7@gmail.com>
2025-01-23 17:39:32 -0500

    update template to latest version of all packages. update tailwindcss configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import svgLoader from 'vite-svg-loader';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(), tailwindcss()],
   server: {
     port: 3000,
     open: true,
