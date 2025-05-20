c09f05df370b80ca1141f79161d7c31ed91189da
benjaminkent <benjamin.jehl7@gmail.com>
2025-05-20 13:51:29 -0400

    update app packages to latest version. add vue devtools vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import svgLoader from 'vite-svg-loader';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader(), tailwindcss()],
+  plugins: [vue(), svgLoader(), tailwindcss(), vueDevTools()],
   server: {
     port: 3000,
     open: true,
